9da8becd05ee02109487aac562fdc314596d8046
ZGamerYT <47927818+ZGamerYT@users.noreply.github.com>
2019-04-01 20:03:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '!'
 
 
-const developers = ["496227815516274698"] 
+const developers = ["445270049389805579"] 
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
